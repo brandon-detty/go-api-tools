319968a1903a90fd4e4f6be14b9a51f01983e332
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2023-03-29 09:29:03 -0700

    use references to items stored in Repo

--- repo/repo.go
+++ repo/repo.go
@@ -3,7 +3,7 @@ package repo
 // A Repo abstracts CRUD operations for a specific persistence strategy;
 // stores items of type T with IDs of type I
 type Repo[T any, I comparable] interface {
-	Set(id I, item T) (T, error)
-	Get(id I) (T, error)
+	Set(id I, item *T) (*T, error)
+	Get(id I) (*T, error)
 	Delete(id I) error
 }
