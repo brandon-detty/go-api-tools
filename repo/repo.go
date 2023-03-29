8d3f3a8fdc5b75fe1c1648d91253c8fee28678a3
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2023-03-29 08:56:08 -0700

    establish basic Repo interface

---
+++ repo/repo.go
@@ -0,0 +1,9 @@
+package repo
+
+// A Repo abstracts CRUD operations for a specific persistence strategy;
+// stores items of type T with IDs of type I
+type Repo[T any, I comparable] interface {
+	Set(id I, item T) (T, error)
+	Get(id I) (T, error)
+	Delete(id I) error
+}
