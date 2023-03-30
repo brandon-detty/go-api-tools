41790eea821e638f11d632fb5049b6b0b4d2a63c
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2023-03-30 13:31:24 -0700

    make mapRepo private; NewMapRepo() covers all uses

--- repo/map_repo.go
+++ repo/map_repo.go
@@ -6,32 +6,32 @@ import (
 	e "github.com/brandon-detty/go-api-tools/entity"
 )
 
-type MapRepo[T e.Entity[I], I comparable] struct {
+type mapRepo[T e.Entity[I], I comparable] struct {
 	store map[I]*T
 }
 
-func NewMapRepo[T e.Entity[I], I comparable]() *MapRepo[T, I] {
+func NewMapRepo[T e.Entity[I], I comparable]() *mapRepo[T, I] {
 	store := make(map[I]*T)
-	r := MapRepo[T, I]{
+	r := mapRepo[T, I]{
 		store,
 	}
 	return &r
 }
 
-func (r *MapRepo[T, I]) Save(item *T) (*T, error) {
+func (r *mapRepo[T, I]) Save(item *T) (*T, error) {
 	id := (*item).Id()
 	r.store[id] = item
 	return item, nil
 }
 
-func (r *MapRepo[T, I]) Get(id I) (*T, error) {
+func (r *mapRepo[T, I]) Get(id I) (*T, error) {
 	if item, ok := r.store[id]; ok {
 		return item, nil
 	}
 	return nil, fmt.Errorf("item with id '%v' not found", id)
 }
 
-func (r *MapRepo[T, I]) Delete(id I) error {
+func (r *mapRepo[T, I]) Delete(id I) error {
 	if _, ok := r.store[id]; !ok {
 		return fmt.Errorf("item with id '%v' not found", id)
 	}
